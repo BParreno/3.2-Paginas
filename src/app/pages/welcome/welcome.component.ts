e42adffdb18396aaf7e165ccdb53a7e6904207e9
BParreno <biparreno@sudamericano.edu.ec>
2024-06-26 08:10:34 -0500

    Commit 2

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Text1Component } from "../../components/text1/text1.component";
+import { Text2Component } from "../../components/text2/text2.component";
+import { Text3Component } from "../../components/text3/text3.component";
 
 @Component({
-  selector: 'app-welcome',
-  standalone: true,
-  templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+    selector: 'app-welcome',
+    standalone: true,
+    templateUrl: './welcome.component.html',
+    styleUrls: ['./welcome.component.css'],
+    imports: [Text1Component, Text2Component, Text3Component]
 })
 export class WelcomeComponent implements OnInit {
 
